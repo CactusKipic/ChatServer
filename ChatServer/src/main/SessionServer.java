c0615810a49f37bc29428a61de707f414d8280bc
CactusKipic <clement.azan@gmail.com>
2020-06-16 19:22:46 +0200

    Ajouts de bases

---
+++ ChatServer/src/main/SessionServer.java
@@ -0,0 +1,15 @@
+package main;
+
+public class SessionServer extends Session{
+	
+	
+	
+	public SessionServer(String Name) {
+		super(Name);
+	}
+	
+	public SessionServer(String Name, String Description) {
+		super(Name, Description);
+	}
+
+}
---
+++ ChatServer/src/main/core/Client.java
@@ -0,0 +1,7 @@
+package main.core;
+
+public class Client {
+	
+	
+	
+}
