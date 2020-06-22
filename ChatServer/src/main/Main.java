8960ad6c503a34a695d1770464cad0f400131397
CactusKipic <clement.azan@gmail.com>
2020-06-22 05:03:27 +0200

    Revert "Mise en place initiale du réseau/com entre le serveur et le client"
    This reverts commit 321f44e7ea35a69a045f9493dcf355f48f291b27.

--- ChatServer/src/main/Main.java
+++ ChatServer/src/main/Main.java
@@ -1,24 +1,10 @@
 package main;
 
-import java.io.File;
-
-import main.network.ServerChat;
-
 public class Main {
-	
-	public static final String PATH = "./Sessions/";
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Démarrage du serveur de Chat");
-		
-		File f = new File(PATH);
-		if(!f.exists()) {
-			f.mkdirs();
-		}
-		ServerChat.launch();
-		
+
 	}
 
 }
